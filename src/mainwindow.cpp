039f8e47db9d5fcaf613e5f2ea03a3dc233cab4d
tlepoix@localhost <thomas.lepoix@protonmail.ch>
2020-08-31 00:59:18 +0200

    GUI : add -n & wrap ui in unique_ptr & some refacto

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -20,69 +20,51 @@
 #include "logger.hpp"
 #include "preview.hpp"
 #include "mainwindow.hpp"
-#include "ui_mainwindow.h"
 using namespace std;
 
 //******************************************************************************
 MainWindow::MainWindow(Data& _data, QWidget *parent) :
-	QMainWindow(parent),
-	ui(new Ui::MainWindow),
-	data(_data),
-	converter(_data),
-	n_sch(QString::fromStdString(_data.n_sch)),
-	out_dir(QString::fromStdString(_data.out_dir)),
-	out_format(QString::fromStdString(_data.out_format)) {
-		ui->setupUi(this);
-		ui->le_path_in->setText(n_sch);
-		ui->le_path_out->setText(out_dir);
-		ui->cb_format->addItem(tr(".kicad_pcb"));
-		ui->cb_format->addItem(tr(".kicad_mod"));
-		ui->cb_format->addItem(tr(".lht"));
-		ui->cb_format->addItem(tr(".m"));
-		ui->cb_format->setCurrentIndex(ui->cb_format->findText(QString::fromStdString(_data.out_format), Qt::MatchExactly));
-		ui->rb_export_whole->setChecked((_data.export_each_block || _data.export_each_subst) ? false : true);
-		ui->rb_export_each_subst->setChecked((_data.export_each_subst && !_data.export_each_block) ? true : false);
-		ui->rb_export_each_block->setChecked((_data.export_each_block) ? true : false);
-		}
-
-//******************************************************************************
-MainWindow::~MainWindow(void) {
-	delete ui;
-	}
-
-//******************************************************************************
-void MainWindow::on_pb_browse_in_clicked(void) {
-	if(this->openfile_path.length() == 0) {
-		this->openfile_path = QDir::currentPath();
-		}
-
-	n_sch=QFileDialog::getOpenFileName(this, tr("Open schematic"), this->openfile_path, tr("Qucs schematic (*.sch)"));
-
-	if(!n_sch.length()) {
-		ui->le_path_in->setText("");
-		return;
-		}
-
-	this->openfile_path = QFileInfo(n_sch).path(); // Store path for next time
+		QMainWindow(parent),
+		ui(std::make_unique<Ui::MainWindow>()),
+		data(_data),
+		converter(_data),
+		n_sch(QString::fromStdString(_data.n_sch)),
+		n_net(QString::fromStdString(_data.n_net)),
+		out_dir(QString::fromStdString(_data.out_dir)),
+		out_format(QString::fromStdString(_data.out_format)),
+		openfile_path(QDir::currentPath()) {
+	ui->setupUi(this);
 	ui->le_path_in->setText(n_sch);
+	ui->le_path_net->setText(n_net);
+	ui->le_path_out->setText(out_dir);
+	ui->cb_format->addItem(tr(".kicad_pcb"));
+	ui->cb_format->addItem(tr(".kicad_mod"));
+	ui->cb_format->addItem(tr(".lht"));
+	ui->cb_format->addItem(tr(".m"));
+	ui->cb_format->setCurrentIndex(ui->cb_format->findText(QString::fromStdString(_data.out_format), Qt::MatchExactly));
+	ui->rb_export_whole->setChecked((_data.export_each_block || _data.export_each_subst) ? false : true);
+	ui->rb_export_each_subst->setChecked((_data.export_each_subst && !_data.export_each_block) ? true : false);
+	ui->rb_export_each_block->setChecked((_data.export_each_block) ? true : false);
+	ui->cb_specify_netlist->setCheckState((_data.n_net=="") ? Qt::Unchecked : Qt::Checked);
 	}
 
 //******************************************************************************
-void MainWindow::on_le_path_in_textChanged(const QString _n_sch) {
-	n_sch=_n_sch;
+void MainWindow::on_cb_format_currentIndexChanged(const QString _out_format) {
+	out_format=_out_format;
 	}
 
 //******************************************************************************
-void MainWindow::on_pb_read_clicked(void) {
-
-	ui->tb_log->clear();
-	if(n_sch=="") {
-		log_err << "ERROR : Nothing to read.\n";
-	} else {
-		converter.clear();
-		converter.reset(n_sch.toStdString(), out_dir.toStdString(), out_format.toStdString());
-		converter.read();
-		ui->glw_preview->set(converter.get_tab_all(), converter.get_extrem_pos());
+void MainWindow::on_cb_specify_netlist_stateChanged(int const state) {
+	if(state==Qt::Unchecked) {
+		ui->l_netlist->setEnabled(false);
+		ui->le_path_net->setEnabled(false);
+		ui->pb_browse_net->setEnabled(false);
+		data.n_net="";
+	} else if(state==Qt::Checked) {
+		ui->l_netlist->setEnabled(true);
+		ui->le_path_net->setEnabled(true);
+		ui->pb_browse_net->setEnabled(true);
+		data.n_net=n_net.toStdString();
 		}
 	}
 
@@ -100,19 +82,14 @@ void MainWindow::on_le_path_in_returnPressed(void) {
 	}
 
 //******************************************************************************
-void MainWindow::on_cb_format_currentIndexChanged(const QString _out_format) {
-	out_format=_out_format;
+void MainWindow::on_le_path_in_textChanged(QString const _n_sch) {
+	n_sch=_n_sch;
 	}
 
 //******************************************************************************
-void MainWindow::on_pb_browse_out_clicked(void) {
-	out_dir=QFileDialog::getExistingDirectory(this, tr("Output directory"), "./", QFileDialog::ShowDirsOnly | QFileDialog::DontResolveSymlinks);
-	ui->le_path_out->setText(out_dir);
-	}
-
-//******************************************************************************
-void MainWindow::on_le_path_out_textChanged(const QString _out_dir) {
-	out_dir=_out_dir;
+void MainWindow::on_le_path_net_textChanged(QString const _n_net) {
+	n_net=_n_net;
+	data.n_net=_n_net.toStdString();
 	}
 
 //******************************************************************************
@@ -127,6 +104,56 @@ void MainWindow::on_le_path_out_returnPressed(void) {
 			}
 	} else {
 		log_err << "ERROR : Nothing to write.\n";
+		}
+	}
+
+//******************************************************************************
+void MainWindow::on_le_path_out_textChanged(QString const _out_dir) {
+	out_dir=_out_dir;
+	}
+
+//******************************************************************************
+void MainWindow::on_pb_browse_in_clicked(void) {
+	n_sch=QFileDialog::getOpenFileName(this, tr("Open schematic"), openfile_path, tr("Qucs schematic (*.sch)"));
+
+	if(!n_sch.length()) {
+		ui->le_path_in->setText("");
+		return;
+		}
+
+	openfile_path=QFileInfo(n_sch).path(); // Store path for next time.
+	ui->le_path_in->setText(n_sch);
+	}
+
+//******************************************************************************
+void MainWindow::on_pb_browse_net_clicked(void) {
+	n_net=QFileDialog::getOpenFileName(this, tr("Open netlist"), openfile_path, tr("Qucs netlist (*.net)"));
+
+	if(!n_net.length()) {
+		ui->le_path_net->setText("");
+		return;
+		}
+
+	openfile_path=QFileInfo(n_net).path(); // Store path for next time.
+	ui->le_path_net->setText(n_net);
+	}
+
+//******************************************************************************
+void MainWindow::on_pb_browse_out_clicked(void) {
+	out_dir=QFileDialog::getExistingDirectory(this, tr("Output directory"), "./", QFileDialog::ShowDirsOnly | QFileDialog::DontResolveSymlinks);
+	ui->le_path_out->setText(out_dir);
+	}
+
+//******************************************************************************
+void MainWindow::on_pb_read_clicked(void) {
+	ui->tb_log->clear();
+	if(n_sch=="") {
+		log_err << "ERROR : Nothing to read.\n";
+	} else {
+		converter.clear();
+		converter.reset(n_sch.toStdString(), out_dir.toStdString(), out_format.toStdString());
+		converter.read();
+		ui->glw_preview->set(converter.get_tab_all(), converter.get_extrem_pos());
 		}
 	}
 
@@ -146,10 +173,10 @@ void MainWindow::on_pb_write_clicked(void) {
 	}
 
 //******************************************************************************
-void MainWindow::on_rb_export_whole_toggled(bool const is_checked) {
+void MainWindow::on_rb_export_each_block_toggled(bool const is_checked) {
 	if(is_checked) {
 		data.export_each_subst=false;
-		data.export_each_block=false;
+		data.export_each_block=true;
 		}
 	}
 
@@ -162,21 +189,21 @@ void MainWindow::on_rb_export_each_subst_toggled(bool const is_checked) {
 	}
 
 //******************************************************************************
-void MainWindow::on_rb_export_each_block_toggled(bool const is_checked) {
+void MainWindow::on_rb_export_whole_toggled(bool const is_checked) {
 	if(is_checked) {
 		data.export_each_subst=false;
-		data.export_each_block=true;
+		data.export_each_block=false;
 		}
 	}
 
 //******************************************************************************
-void MainWindow::keyPressEvent(QKeyEvent *event) {
+void MainWindow::keyPressEvent(QKeyEvent* event) {
 	if(event->key()==Qt::Key_Control) ui->glw_preview->setFCtrl(true);
 	if(event->key()==Qt::Key_Shift) ui->glw_preview->setFShift(true);
 	}
 
 //******************************************************************************
-void MainWindow::keyReleaseEvent(QKeyEvent *event) {
+void MainWindow::keyReleaseEvent(QKeyEvent* event) {
 	if(event->key()==Qt::Key_Control) ui->glw_preview->setFCtrl(false);
 	if(event->key()==Qt::Key_Shift) ui->glw_preview->setFShift(false);
 	}
--- src/mainwindow.hpp
+++ src/mainwindow.hpp
@@ -21,16 +21,13 @@
 #include <QMainWindow>
 #include <QObject>
 
+#include <memory>
 #include <sstream>
 
 #include "converter.hpp"
 #include "data.hpp"
 #include "logger.hpp"
-
-namespace Ui {
-//******************************************************************************
-class MainWindow;
-} // namespace Ui
+#include "ui_mainwindow.h"
 
 //******************************************************************************
 class MainWindow : public QMainWindow, public Loggable {
@@ -38,32 +35,36 @@ class MainWindow : public QMainWindow, public Loggable {
 
 public:
 	explicit MainWindow(Data& _data, QWidget* parent=0);
-	~MainWindow(void);
+	~MainWindow(void)=default;
 
 private slots:
-	void on_pb_read_clicked(void);
-	void on_le_path_in_textChanged(QString const _n_sch);
-	void on_le_path_in_returnPressed(void);
 	void on_cb_format_currentIndexChanged(QString const _out_format);
-	void on_pb_browse_in_clicked(void);
-	void on_pb_browse_out_clicked(void);
-	void on_le_path_out_textChanged(QString const _out_dir);
+	void on_cb_specify_netlist_stateChanged(int const state);
+	void on_le_path_in_returnPressed(void);
+	void on_le_path_in_textChanged(QString const _n_sch);
+	void on_le_path_net_textChanged(QString const _n_net);
 	void on_le_path_out_returnPressed(void);
+	void on_le_path_out_textChanged(QString const _out_dir);
+	void on_pb_browse_in_clicked(void);
+	void on_pb_browse_net_clicked(void);
+	void on_pb_browse_out_clicked(void);
+	void on_pb_read_clicked(void);
 	void on_pb_write_clicked(void);
-	void on_rb_export_whole_toggled(bool const is_checked);
-	void on_rb_export_each_subst_toggled(bool const is_checked);
 	void on_rb_export_each_block_toggled(bool const is_checked);
+	void on_rb_export_each_subst_toggled(bool const is_checked);
+	void on_rb_export_whole_toggled(bool const is_checked);
 
 protected:
 	void keyPressEvent(QKeyEvent* event) override;
 	void keyReleaseEvent(QKeyEvent* event) override;
 
 private:
-	Ui::MainWindow* ui;
+	std::unique_ptr<Ui::MainWindow> ui;
 	Data& data;
 	Converter converter;
 
 	QString n_sch;
+	QString n_net;
 	QString out_dir;
 	QString out_format;
 	QString n_out;
