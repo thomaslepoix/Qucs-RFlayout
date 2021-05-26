a1855f1384683d77d863db4b87a55adea899bfe1
thomaslepoix <thomas.lepoix@protonmail.ch>
2021-05-26 07:58:39 +0200

    fix typo

--- cmake/FixedShell.cmake
+++ cmake/FixedShell.cmake
@@ -1,8 +1,7 @@
 # In a command such as add_test(), add_custom_target(), etc.
 # The pattern `COMMAND <bin> <arg> <arg>` has some oddities, for example
 # `2> /dev/null` does not work properly. Instead you can prefer this pattern
-# `COMMAND ${FIXED_SHELL} "<bin> <arg> <arg>`
+# `COMMAND ${FIXED_SHELL} "<bin> <arg> <arg>"`
 ################################################################################
 
 set( FIXED_SHELL ${CMAKE_COMMAND} -E env bash -c )
-
